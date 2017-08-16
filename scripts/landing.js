4ca8c90b0b1405a660b13964bf48983ee5f2e33a
ltcbg <lodentashi@hotmail.com>
2017-08-16 02:33:33 +0000

    Checkpoint 29

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,5 +1,7 @@
+ /* global $ */
  var points = document.getElementsByClassName('point');
  
+ /*
  var animatePoints = function(points) {
  
      var revealPoint = function(){
@@ -12,19 +14,31 @@
      } 
      revealPoint();
  };
+ */
  
- window.onload = function() {
-     animatePoints(points);
-     /*
-     if (window.innerHeight > 950) {
-         animatePoints(points);
+ var animatePoints = function() {
+     var revealPoint = function() {
+         $(this).css({
+             opacity: 1,
+             transform: 'scaleX(1) translateY(0)'
+         });
+     };
+     $.each($('.point'), revealPoint);
+ };
+ 
+ $(window).load(function() {
+
+     if ($(window).height() > 950) {
+         animatePoints();
      }
-     var sellingPoints = document.getElementsByClassName('selling-points')[0];
-     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
-     window.addEventListener('scroll', function(event) {
-          if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) { 
-             animatePoints(points);   
+     
+     var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
+     
+     $(window).scroll(function(event) {
+
+          if ($(window).scrollTop() >= scrollDistance) {
+             animatePoints();
           }
      });
-     */
- }
+     
+ });
